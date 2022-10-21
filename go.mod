32223ebf90c1fe5ee49ca6dd2a68838e4b331a6b
thanoz <carlos-7@hotmail.de>
2022-10-21 13:14:21 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thannoz/snippetbox
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
