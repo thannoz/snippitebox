c0f8cfa7726d6ad7f2ba7dc7c4a91f34f79c460e
thanoz <carlos-7@hotmail.de>
2022-10-25 17:18:13 +0200

    Added functionality for processing forms. Parsing & validating form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/thannoz/snippetbox/internal/models"
@@ -50,15 +52,69 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	app.render(w, http.StatusOK, "create.gohtml", data)
 	w.Write([]byte("Displaying a form to create a new snippet"))
 }
 
-func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+// Define a snippetCreateForm struct to represent the form data and validation
+// errors for the form fields.
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
 
-	id, err := app.snippets.InsertSnippet(title, content, expires)
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
+	// Initialize a map to hold any validation errors for the form fields.
+	fieldsErrors := make(map[string]string)
+
+	if strings.TrimSpace(form.Title) == "" {
+		fieldsErrors["title"] = "This field cannot be empty"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		fieldsErrors["title"] = "This field cannot be more than 100 characters"
+	}
+
+	if strings.TrimSpace(form.Content) == "" {
+		fieldsErrors["content"] = "This field cannot be empty"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldsErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	// If there are any validation errors re-display the create.tmpl template,
+	// passing in the snippetCreateForm instance as dynamic data in the Form
+	// field.
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.gohtml", data)
+		return
+	}
+
+	id, err := app.snippets.InsertSnippet(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,6 +14,7 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+	Form        any
 }
 
 // humanDate returns a nicely formatted date
